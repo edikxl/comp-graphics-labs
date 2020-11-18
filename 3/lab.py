80d0850d66b4943c12960b23f896c93e8a23fb2e
edikxl <edik24032002@gmail.com>
2020-11-18 17:09:36 +0200

    3

---
+++ 3/lab.py
@@ -0,0 +1,70 @@
+from typing import List
+import sys
+
+import cv2
+import numpy as np
+from PIL import Image
+
+def getDataFromFile(path: str) -> str:
+    with open(path, 'r') as file:
+        return file.read()
+
+def getCordsFromData(data: str) -> set:
+    x = []
+    y = []
+
+    data = data.split('\n') 
+    for string in data:
+        if string:
+            x_, y_ = string.split(' ')
+
+            x.append(int(x_))
+            y.append(int(y_))
+
+    return x, y
+
+def getImageArrayFromCords(x: List[int], y: List[int], color: List[int]) -> list:
+    imageArray = np.zeros([ max(y) + 1, max(x) + 1, 4], dtype=np.uint8)
+
+    for i in range(len(x)):
+        imageArray[y[i]][x[i]] = color
+    
+    return imageArray
+
+def enchanceImage(image):
+    image = image.resize((540, 960), Image.LANCZOS)
+    image = image.rotate(90, expand=True)
+
+    return image
+
+def changeExtToPng(path: str) -> str:
+    return path[:path.find('.') + 1] + 'png'
+
+def convexHullFromImage(x: List[int], y: List[int], savePath: str, lineColor: set) -> str:
+    dots = [ [[x[i], y[i]]] for i in range(len(x)) ]
+    dots = np.array(dots)
+    hull = cv2.convexHull(dots)
+
+    img = cv2.imread(savePath, -1)
+    cv2.drawContours(img, [hull], -1, lineColor, 2)
+    cv2.imwrite(savePath, img)
+
+    return Image.open(savePath)
+
+if __name__ == '__main__':
+    COLOR = [0, 0, 0, 255]
+    LINE_COLOR = [255, 0, 0, 255]
+
+    datasetPath = sys.argv[1]
+    savePath = changeExtToPng(datasetPath)
+
+    data = getDataFromFile(datasetPath)
+    x, y = getCordsFromData(data)
+    imageArray = getImageArrayFromCords(x, y, COLOR)
+
+    image = Image.fromarray(imageArray)
+    image.save(savePath)
+
+    image = convexHullFromImage(x, y, savePath, LINE_COLOR)
+    image = enchanceImage(image)
+    image.save(savePath)
