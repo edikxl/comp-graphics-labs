25bc08c3f04f7ec8e663733e067ea4ccedde400a
edikxl <edik24032002@gmail.com>
2020-11-28 14:21:28 +0200

    Lab 2 Res Fix

--- 2/lab.py
+++ 2/lab.py
@@ -4,15 +4,17 @@ import sys
 import numpy as np
 from PIL import Image
 
+
 def getDataFromFile(path: str) -> str:
     with open(path, 'r') as file:
         return file.read()
+
 
 def getCordsFromData(data: str) -> set:
     x = []
     y = []
 
-    data = data.split('\n') 
+    data = data.split('\n')
     for string in data:
         if string:
             x_, y_ = string.split(' ')
@@ -22,31 +24,37 @@ def getCordsFromData(data: str) -> set:
 
     return x, y
 
-def getImageArrayFromCords(x: List[int], y: List[int], color: List[int]) -> list:
-    imageArray = np.zeros([ max(y) + 1, max(x) + 1, 4], dtype=np.uint8)
+
+def getImageArrayFromCords(width: int, height: int, rgb: int, x: List[int], y: List[int], color: List[int]) -> list:
+    imageArray = np.zeros([height, width, rgb], dtype=np.uint8)
 
     for i in range(len(x)):
         imageArray[y[i]][x[i]] = color
-    
+
     return imageArray
 
+
 def enchanceImage(image):
-    image = image.resize((540, 960), Image.LANCZOS)
     image = image.rotate(90, expand=True)
 
     return image
 
+
 def changeExtToPng(path: str) -> str:
     return path[:path.find('.') + 1] + 'png'
 
+
 if __name__ == '__main__':
+    WIDTH = 540
+    HEIGHT = 960
+    RGB = 4  # RGBA
     COLOR = [0, 0, 0, 255]
 
     datasetPath = sys.argv[1]
 
     data = getDataFromFile(datasetPath)
     x, y = getCordsFromData(data)
-    imageArray = getImageArrayFromCords(x, y, COLOR)
+    imageArray = getImageArrayFromCords(WIDTH, HEIGHT, RGB, x, y, COLOR)
 
     image = Image.fromarray(imageArray)
     image = enchanceImage(image)
